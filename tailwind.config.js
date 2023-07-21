3ccb2326aae8f144f3f68fc72f6f6a37e7d81f44
adalbert353 <vojtech.votruba@seznam.cz>
2023-07-21 16:19:49 +0200

    added tailwind, removed bootstrap

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./pages/personal.html", "./pages/work.html"],
+  theme: {
+    extend: {
+      colors: {
+				smoke: '#e2e8f0',
+				darkish: '#1c1917',
+				lightish: '#f1f5f9',
+				emerald: '#047857',
+			  },
+      fontFamily: {
+        libre: ['"Libre Baskerville"', ...defaultTheme.fontFamily.sans]
+      },
+    },
+  },
+  plugins: [],
+}
