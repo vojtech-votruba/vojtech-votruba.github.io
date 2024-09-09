efa5edab61c24d82e1ec683cfccec7bad76ed634
adalbert353 <vojtech.votruba@seznam.cz>
2024-09-09 17:52:59 +0200

    changed tailwind config, added layouts, added components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+		  colors: {
+					smoke: '#e2e8f0',
+					darkish: '#1c1917',
+					lightish: '#f1f5f9',
+					emerald: '#059669',
+					cyan: '#22d3ee',
+				  },
+			fontFamily: {
+			  libre: "'Libre Baskerville', serif",
+			}
+		},
+	  },
 	plugins: [],
 }
