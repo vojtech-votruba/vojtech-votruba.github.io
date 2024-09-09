55933692632935bc8a800eb8c79af7392327ddf5
Vojtěch Votruba <vv.votruba@gmail.com>
2024-09-09 23:07:44 +0200

    trying to deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://vojtech-votruba.github.io'
 });
