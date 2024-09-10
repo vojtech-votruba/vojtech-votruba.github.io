9e0522b284315bc60c17d29603edff98b6cd4dd3
Vojtěch Votruba <vv.votruba@gmail.com>
2024-09-10 08:28:01 +0200

    fixed tailwind support, i hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://vojtech-votruba.github.io',
-  output: 'static'
 });
